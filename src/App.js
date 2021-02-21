5ed15fde0395f58f06d6fa28b2682a8bd4649b33
AyazAI <ayaz.uiit3@gmail.com>
2021-02-21 23:13:06 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  incrementCounter,
+  decrementCounter,
+  fetchDataSuccess,
+  fetchData,
+} from "./redux/actions";
+import { connect } from "react-redux";
 
-function App() {
+function App({
+  incrementCounterAction,
+  decrementCounterAction,
+  fetchDataAction,
+  data,
+}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <h1>Fetch Some data ?</h1>
+      <button onClick={() => fetchDataAction()}>Fetch Data</button>
+      {data && console.log(data)}
+      {data &&
+        data.map((record) => {
+          return <li key={record.id}>{record.body}</li>;
+        })}
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = ({ posts }) => {
+  const { data } = posts;
+  return {
+    data,
+  };
+};
+
+export default connect(mapStateToProps, {
+  incrementCounterAction: incrementCounter,
+  decrementCounterAction: decrementCounter,
+  fetchDataAction: fetchData,
+})(App);
